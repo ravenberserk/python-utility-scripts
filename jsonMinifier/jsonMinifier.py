e844828d3ce4f10b9f7ade37fe06ae4378b4af6c
Raven <raven.berserk@gmail.com>
2017-05-22 17:50:23 +0200

    Adjust naming of variables and methods to the naming convention

--- jsonMinifier/jsonMinifier.py
+++ jsonMinifier/jsonMinifier.py
@@ -10,18 +10,24 @@ __maintainer__ = "Javier Grande Pérez"
 __email__ = "raven.berserk@gmail.com"
 __status__ = "Production"
 
-def read_json(jsonPath:str)->dict:
+
+def read_json(json_path: str)->dict:
     """ Auxiliar method, that will read the json file from a URL. """
-    
-    response = requests.get(jsonPath)
+
+    response = requests.get(json_path)
     return response.json() if response.ok else response.raise_for_status()
 
-def json_minifier():
+
+def main():
     """ Main method, that will be responsible for minifier the json file. """
 
     # Fist, it is needed get the json file from the URL.
-    jsonFile = read_json(input('Please indicate the json file URL: '))
+    json_file = read_json(input('Please indicate the json file URL: '))
 
     # Print the minify json file.
-    print(json.dumps(jsonFile,separators=(',',':')))
+    print(json.dumps(json_file, separators=(',', ':')))
     print('Process completed successfully :)')
+
+
+if __name__ == '__main__':
+    main()
--- redmineTrelloIntegration/redmineCardCreation.py
+++ redmineTrelloIntegration/redmineCardCreation.py
@@ -19,106 +19,131 @@ URL_REMINE = 'http://redmine.org/redmine/issues/'
 URL_API_REMINE = 'http://redmine.org/redmine/issues/{0}.json?key={1}'
 URL_TRELLO = 'https://api.trello.com/1/{0}?key={1}&token={2}'
 
+
 class RedmineTask:
     pass
 
-def create_dashboard(taskId:str,taskName:str) -> str:
+
+def create_dashboard(task_id: str, task_name: str)-> str:
     """ Create a new dashboard in Trello. """
-    
-    jsonDatos = {"name":'{0} - {1}'.format(taskId,taskName)}
-    response = requests.post(URL_TRELLO.format('boards',TRELLO_API_KEY,TRELLO_TOKEN),json=jsonDatos)
-    return response.json()['id'] if response.ok else response.raise_for_status()
 
-def get_default_list_dashboard(dashboardId:str) -> str:
+    json_datos = {"name": '{0} - {1}'.format(task_id, task_name)}
+
+    resp = requests.post(URL_TRELLO.format('boards', TRELLO_API_KEY,
+                                           TRELLO_TOKEN), json=json_datos)
+
+    return resp.json()['id'] if resp.ok else resp.raise_for_status()
+
+
+def get_default_list_dashboard(dashboard_id: str)-> str:
     """ Get de first list of the dashboard. """
-    
-    response = requests.get(URL_TRELLO.format('boards/{0}/lists'.format(dashboardId),TRELLO_API_KEY,TRELLO_TOKEN))
-    return response.json()[0]['id'] if response.ok else response.raise_for_status()
 
-def create_new_trello_card(redmineTask:RedmineTask, dashListId:str):
+    resp = requests.get(URL_TRELLO.format('boards/{0}/lists'
+                                          .format(dashboard_id),
+                                          TRELLO_API_KEY, TRELLO_TOKEN))
+
+    return resp.json()[0]['id'] if resp.ok else resp.raise_for_status()
+
+
+def create_new_trello_card(task: RedmineTask, dash_list_id: str):
     """ Create a new card with task information. """
 
-    cardName = 'HU#{0} - {1}'.format(redmineTask.id, redmineTask.name)
-    cardDescription = 'HU#{0}: {1}/{0}'.format(redmineTask.id, URL_REMINE)
-    endDate = '&due={0}'.format(redmineTask.endDate) if redmineTask.endDate else ''
+    card_name = 'HU#{0} - {1}'.format(task.id, task.name)
+    card_description = 'HU#{0}: {1}/{0}'.format(task.id, URL_REMINE)
+    end_date = '&due={0}'.format(task.endDate) if task.endDate else ''
 
-    jsonDatos = {"name":cardName,
-            "idList":dashListId,
-            "desc":cardDescription,
-            "due":endDate}
-    requests.post(URL_TRELLO.format('cards',TRELLO_API_KEY,TRELLO_TOKEN),json=jsonDatos)
+    json_datos = {"name": card_name,
+                  "idList": dash_list_id,
+                  "desc": card_description,
+                  "due": end_date}
 
-def get_redmine_task_info(taskId:str) -> dict:
+    requests.post(URL_TRELLO.format('cards', TRELLO_API_KEY, TRELLO_TOKEN),
+                  json=json_datos)
+
+
+def get_redmine_task_info(task_id: str)-> dict:
     """ Get the Redmine Task information. """
 
-    response = requests.get(URL_API_REMINE.format(taskId,REDMINE_API_KEY)+'&include=children')
-    return response.json()['issue'] if response.ok else response.raise_for_status()
+    url_get_children = URL_API_REMINE + '&include=children'
+    resp = requests.get(url_get_children.format(task_id, REDMINE_API_KEY))
 
-def check_task_is_open(task:dict) -> bool:
+    return resp.json()['issue'] if resp.ok else resp.raise_for_status()
+
+
+def check_task_is_open(task: dict)-> bool:
     """ Check if the Redmine task is Open. """
     return 'closed_on' not in task or not task['closed_on']
 
-def parse_task(redmineTask:dict) -> RedmineTask:
+
+def parse_task(redmine_task: dict)-> RedmineTask:
     """ Create a new RedmineTask from a json. """
-    
+
     task = RedmineTask()
-    task.id = redmineTask['id']
-    task.name = redmineTask['subject']
-    task.endDate = redmineTask.get('due_date')
+    task.id = redmine_task['id']
+    task.name = redmine_task['subject']
+    task.endDate = redmine_task.get('due_date')
     return task
 
-def get_children_tasks(parentTask:dict) -> list:
+
+def get_children_tasks(parent_task: dict)-> list:
     """ Get the information from the children task of the task. """
 
-    childrenList = []
+    children_list = []
 
-    for child in parentTask['children']:
-        childTask = get_redmine_task_info(child['id'])
+    for child in parent_task['children']:
+        child_task = get_redmine_task_info(child['id'])
 
-        if check_task_is_open(childTask):
-            if 'children' not in childTask:
+        if check_task_is_open(child_task):
+            if 'children' not in child_task:
                 # The task has not children, so it is added to the list.
-                childrenList.append(parse_task(childTask))
+                children_list.append(parse_task(child_task))
             else:
-                # The task has children, so the method is called again to pick his children.
-                childrenList = childrenList + get_children_tasks(childTask)
+                # The task has children, so the method is
+                # called again to pick his children.
+                children_list += get_children_tasks(child_task)
 
-    return childrenList
+    return children_list
 
-def redmine_card_creation():
+
+def main():
     """ Main method, will be responsible for creating the task cards. """
-    
+
     try:
-        taskId = input('Indicate the task you want to migrate: ')
-        mainTask = get_redmine_task_info(taskId)
+        task_id = input('Indicate the task you want to migrate: ')
+        main_task = get_redmine_task_info(task_id)
 
-        if 'children' in mainTask:
-            listChildrenTasks = get_children_tasks(mainTask)
+        if 'children' in main_task:
+            list_children_tasks = get_children_tasks(main_task)
 
-            if listChildrenTasks:
-                print('There are {0} sub-task. The creation of the dashboard and cards begins')
+            if list_children_tasks:
+                print('There are {0} sub-task.'
+                      .format(len(list_children_tasks)),
+                      'The creation of the dashboard and cards begins')
 
                 print('--- The creation of the dashboard begins.')
-                dashboardId = create_dashboard(mainTask['id'],mainTask['subject'])
+                dashboard_id = create_dashboard(main_task['id'],
+                                                main_task['subject'])
 
                 print('--- The default list is retrieved.')
-                listId = get_default_list_dashboard(dashboardId)
+                list_id = get_default_list_dashboard(dashboard_id)
 
                 print('--- The creation of the cards begins.')
-                for task in listChildrenTasks:
+                for task in list_children_tasks:
                     try:
-                        create_new_trello_card(task,listId)
+                        create_new_trello_card(task, list_id)
                     except Exception as error:
-                        print('--- There was an error creating the card of the task {0}: {1}'.format(task.id, error))
+                        print('--- Error creating the card of the task {0} '
+                              .format(task.id), error)
             else:
-                print('The task {0} has not open sub-tasks'.format(taskId))
+                print('The task {0} has not open sub-tasks'.format(task_id))
         else:
-            print('The task {0} has not sub-task.'.format(taskId))
+            print('The task {0} has not sub-task.'.format(task_id))
 
         input('The process completed successfully. :)')
     except Exception as error:
         print("Error {0}".format(error))
         input('Process not completed. :(')
 
+
 if __name__ == '__main__':
-    redmine_card_creation()
+    main()
